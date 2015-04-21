70c6b87fe72118724d413d74ba36003c128fc6cf
calculator1 <alenyhka18@yandex.ru>
2015-04-21 18:34:43 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,32 @@
-<?php
-$ups = array(1, 2, -5 , 3 , 4 , 5,  6);
-arsort($ups);
-foreach ($ups as $key => $val) {
-    echo "$key = $val\n";
+<?php 
+class Cat {   
+ 
+// Возраст кота   
+ var $age;
+  function Cat($new_age)    
+  {    
+    
+// Установить возраст этого кота в новое значение        
+$this->age = $new_age;    
+}
+function Birthday()    
+{        
+$this->age++;    
+} 
+}
+class Domestic_Cat extends Cat {    
+
+// Конструктор   
+ function Domestic_Cat() {    
+ }
+    
+// Спать, как домашний кот   
+ function sleep() {       
+ echo("Хр-р-р.<br />");    
+ } 
 }
 
-$ups = array( 1, 2, -5 , 3 , 4 , 5,  6 );
-function getSum ( $a ) {
- 
-$sum = 0;
-foreach ( $a as $val )
-        ( $val >= 0 ) &&
-        ( $sum += $val );
- 
-return $sum;
-}
-echo getSum ( $ups );
-
+$fluffy = new Domestic_Cat(); 
+$fluffy->Birthday(); $fluffy->sleep(); 
+echo "Коту $fluffy->age год<br>/"; 
 ?>
-
